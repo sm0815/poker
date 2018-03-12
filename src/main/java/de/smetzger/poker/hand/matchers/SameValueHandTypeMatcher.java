addfae65040c1c610e85a0479b7d4d411359c021
smetzger <job@smetzger.de>
2018-03-12 16:06:28 +0100

    Moved indiviaul hand type generation in SameValueMatcher into separate methods

--- src/main/java/de/smetzger/poker/hand/matchers/SameValueHandTypeMatcher.java
+++ src/main/java/de/smetzger/poker/hand/matchers/SameValueHandTypeMatcher.java
@@ -28,54 +28,83 @@ public class SameValueHandTypeMatcher implements PokerHandTypeMatcher {
         Map<PokerCardValue, Set<PokerCard>> cardsByValue = pokerHand.getHand().stream()
                 .collect(Collectors.groupingBy(PokerCard::getValue, Collectors.toSet()));
 
-        // no duplicate values, this matcher has nothing to find
-        if(cardsByValue.size()==5){
+        switch (cardsByValue.size()) {
+        case 5:
+            // no duplicate values, this matcher has nothing to find
             return null;
+        case 4:
+            return handlePairHandType(pokerHand, cardsByValue);
+        case 3:
+            // three groups is either a double pair and a single value or a triplet and two single values
+            return handleThreeOfAKindOrDoublePair(pokerHand, cardsByValue);
+
+        case 2:
+            // if we have only two groups it could be a full house or four and a single value
+            return handleFourOfAKindOrFullHouse(pokerHand, cardsByValue);
+
+        default:
+            LOGGER.error("Unexpected number of different values ({}) on hand {}:{}", cardsByValue.size(), pokerHand,
+                    cardsByValue);
+            throw new IllegalStateException(
+                    "There should always be 1 to 5 cardsByValue groups, now there are " + cardsByValue.size());
         }
 
+    }
+
+    private PokerHandType handleFourOfAKindOrFullHouse(PokerHand pokerHand,
+            Map<PokerCardValue, Set<PokerCard>> cardsByValue) {
+        Set<PokerCard> cardSetWithMaxSize = cardsByValue.values().stream()
+                .max((cardsetA, cardsetB) -> Integer.compare(cardsetA.size(), cardsetB.size())).get();
+        if (cardSetWithMaxSize.size() == 4) {
+            return handleFourOfAKind(pokerHand, cardSetWithMaxSize);
+        }
+
+        return handleFullHouse(pokerHand, cardsByValue);
+    }
+
+    private PokerHandType handleThreeOfAKindOrDoublePair(PokerHand pokerHand,
+            Map<PokerCardValue, Set<PokerCard>> cardsByValue) {
+        Set<PokerCard> cardSetWithMaxSize = cardsByValue.values().stream()
+                .max((cardsetA, cardsetB) -> Integer.compare(cardsetA.size(), cardsetB.size())).get();
+        if (cardSetWithMaxSize.size() == 3) {
+            return handleThreeOfAKind(pokerHand, cardSetWithMaxSize);
+        }
+        return handleDoublePair(pokerHand, cardsByValue);
+    }
+
+    private PokerHandType handleFullHouse(PokerHand pokerHand, Map<PokerCardValue, Set<PokerCard>> cardsByValue) {
+        PokerCardValue tripletValue = findXOfAKindValue(cardsByValue, 3);
+        return new FullHouseHandType(pokerHand, tripletValue);
+    }
+
+    private PokerHandType handleFourOfAKind(PokerHand pokerHand, Set<PokerCard> cardSetWithMaxSize) {
+        PokerCardValue quartetValue = cardSetWithMaxSize.iterator().next().getValue();
+        return new FourOfAKindHandType(pokerHand, quartetValue);
+    }
+
+    private PokerHandType handleDoublePair(PokerHand pokerHand, Map<PokerCardValue, Set<PokerCard>> cardsByValue) {
+        List<PokerCardValue> pairValues = cardsByValue.entrySet().stream().filter(entry -> entry.getValue().size() == 2)
+                .map(entry -> entry.getKey()).collect(Collectors.toList());
+        PokerCardValue singleCardValue = findXOfAKindValue(cardsByValue, 1);
+
+        return new DoublePairHandType(pokerHand, pairValues, singleCardValue);
+    }
+
+    private PokerHandType handleThreeOfAKind(PokerHand pokerHand, Set<PokerCard> cardSetWithMaxSize) {
+        PokerCardValue tripletValue = cardSetWithMaxSize.iterator().next().getValue();
+        return new ThreeOfAKindHandType(pokerHand, tripletValue);
+    }
+
+    private PokerHandType handlePairHandType(PokerHand pokerHand, Map<PokerCardValue, Set<PokerCard>> cardsByValue) {
         // if there are four groups of values, one value has to be duplicated
-        if(cardsByValue.size()==4){
-            PokerCardValue pairvalue=findXOfAKindValue(cardsByValue,2);
-            return new PairHandType(pokerHand, pairvalue);
-        }
-
-        // if we have only two groups it could be a full house or four and a single value
-        if(cardsByValue.size()==2){
-            Set<PokerCard> cardSetWithMaxSize = cardsByValue.values().stream()
-                    .max((cardsetA, cardsetB) -> Integer.compare(cardsetA.size(), cardsetB.size())).get();
-            if (cardSetWithMaxSize.size() == 4) {
-                PokerCardValue quartetValue = cardSetWithMaxSize.iterator().next().getValue();
-                return new FourOfAKindHandType(pokerHand, quartetValue);
-            }
-
-            PokerCardValue tripletValue=findXOfAKindValue(cardsByValue,3);
-            return new FullHouseHandType(pokerHand, tripletValue);
-        }
-
-        // three groups is either a double pair and a single value or a triplet and two single values
-        if(cardsByValue.size()==3){
-            Set<PokerCard> cardSetWithMaxSize = cardsByValue.values().stream()
-                    .max((cardsetA, cardsetB) -> Integer.compare(cardsetA.size(), cardsetB.size())).get();
-            if (cardSetWithMaxSize.size() == 3) {
-                PokerCardValue tripletValue = cardSetWithMaxSize.iterator().next().getValue();
-                return new ThreeOfAKindHandType(pokerHand, tripletValue);
-            }
-            List<PokerCardValue> pairValues= cardsByValue.entrySet().stream()
-                    .filter(entry -> entry.getValue().size() == 2).map(entry -> entry.getKey())
-                    .collect(Collectors.toList());
-            PokerCardValue singleCardValue = findXOfAKindValue(cardsByValue, 1);
-
-            return new DoublePairHandType(pokerHand, pairValues, singleCardValue);
-        }
-        LOGGER.error("Unexpected number of different values ({}) on hand {}:{}", cardsByValue.size(), pokerHand,
-                cardsByValue);
-        return null;
+        PokerCardValue pairvalue = findXOfAKindValue(cardsByValue, 2);
+        return new PairHandType(pokerHand, pairvalue);
     }
 
     private PokerCardValue findXOfAKindValue(Map<PokerCardValue, Set<PokerCard>> cardsByValue,
             int amountOfCardsWithSharedValue) {
-        return cardsByValue.entrySet().stream()
-                .filter(entry -> entry.getValue().size() == amountOfCardsWithSharedValue).findAny().get().getKey();
+        return cardsByValue.entrySet().stream().filter(entry -> entry.getValue().size() == amountOfCardsWithSharedValue)
+                .findAny().get().getKey();
     }
 
 }
